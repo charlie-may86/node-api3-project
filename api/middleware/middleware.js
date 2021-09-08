e53b4cdde8cac9297ee06f418dc201fc57262c9b
Charlie May <61992595+charlie-may86@users.noreply.github.com>
2021-09-08 11:25:47 -0500

    server working, middleware connected

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,9 +1,28 @@
+const Users = require("../users/users-model");
+
 function logger(req, res, next) {
   // DO YOUR MAGIC
+
+  console.log(`This is the request method: ${req.method}`);
+  console.log(`This is the request url: ${req.url}`);
+  console.log(`This is when the request took place: ${req.timestamp}`);
+  next();
 }
 
 function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+  const id = req.params.id;
+  console.log("this is the validate thing");
+  Users.getById(id)
+    .then((user) => {
+      if (user) {
+        console.log(user);
+        console.log(req);
+        next();
+      } else {
+        next({ message: `user with id: ${id} not found` });
+      }
+    })
+    .catch(next);
 }
 
 function validateUser(req, res, next) {
@@ -15,3 +34,5 @@ function validatePost(req, res, next) {
 }
 
 // do not forget to expose these functions to other modules
+
+module.exports = { logger, validateUserId };
